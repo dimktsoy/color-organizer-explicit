e4fdffcf92d979ea9b6122c03b8bf820a2553267
Dima Tsoy <dimktsoy@yahoo.com>
2020-08-26 11:38:13 +0600

    Update index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, combineReducers } from 'redux';
+import { colorsReducer } from './store/reducers';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  combineReducers({
+    colors: colorsReducer,
+  })
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+window.store = store;
+
+const render = () =>
+  ReactDOM.render(<App store={store} />, document.getElementById('root'));
+
+store.subscribe(render);
+
+render();
